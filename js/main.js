1254bd9e1e3a6b69ab9bddeeafcf8894618e48dd
W1L74680 <williammiclette@icloud.com>
2022-10-04 11:49:54 -0400

    init

---
+++ js/main.js
@@ -0,0 +1,24 @@
+function hamburgerMenu() {
+    let x = document.getElementById("nav");
+    if (x.className === "nav") {
+        x.className += " responsive";
+    } else {
+        x.className = "nav";
+    }
+}
+
+function searchMenu() {
+    let input, filter, ul, li, a, i;
+    input = document.getElementById("search");
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("menu");
+    li = ul.getElementsByTagName("li");
+    for (i = 0; i < li.length; i++) {
+        a = li[i].getElementsByTagName("a")[0];
+        if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = "";
+        } else {
+            li[i].style.display = "none";
+        }
+    }
+}
